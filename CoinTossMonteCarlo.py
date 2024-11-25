fe492d225ad149bae214f7943417bb55c34e0abf
zhiyingTan0 <50588149+zhiyingTan0@users.noreply.github.com>
2024-11-24 20:40:49 -0500

    BinomialDist

--- CoinTossMonteCarlo.py
+++ CoinTossMonteCarlo.py
@@ -6,10 +6,12 @@ p=0.5 # probability of Head
 k=5 # at least k Head of N toss
 trials=10000 # number of experiments
 
-# generate N random numbers uniformly from [0,1)
+# Method 1: Binomial distribution: N number of  Bernoulli distribution, 1 is Head and 0 is Tail
+# Method 2: np.random.rand(N): generate N random numbers uniformly from [0,1)
 # <.5 is T  >=.5 is H
 def toss_N_times(N, p):
-    return np.sum(np.random.rand(N) < p)
+    #return np.sum(np.random.rand(N) < p)
+    return np.sum(np.random.binomial(1, p, N))
 def coin_toss_monte_carlo(N, k, p, trials):
     at_least_k = 0
     for _ in range(trials):
@@ -17,7 +19,9 @@ def coin_toss_monte_carlo(N, k, p, trials):
             at_least_k += 1
     return at_least_k/trials
 
-print(f"Perform {trials} trials, probability of at least {k} Heads in {N} toss is: {coin_toss_monte_carlo(N, k, p, trials)}")
+print(f"Perform {trials} trials, probability of at least {k} Heads in {N} toss is: "
+      f"{coin_toss_monte_carlo(N, k, p, trials): .5f}")
+
 
 
 
